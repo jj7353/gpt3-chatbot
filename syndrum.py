381c5730c5b0a422394b7214b329639d3ea1f013
jj7353 <jj7353@gmail.com>
2022-06-21 01:07:34 -0500

    Update syndrum.py

--- syndrum.py
+++ syndrum.py
@@ -8,21 +8,21 @@ load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 completion = openai.Completion()
 
-start_sequence = "\nJabe:"
+start_sequence = "\nJade:"
 restart_sequence = "\n\nPerson"
-session_prompt = "Lets begin by introducing yourself."
+session_prompt = "You are talking with Jade, AI bot.  Ask anything you want."
 
 def ask(question, chat_log=None):
     prompt_text = f'{chat_log}{restart_sequence}: {question}{start_sequence}:'
     response = openai.Completion.create(
-      engine="davinci",
+      model="text-davinci-002",
       prompt=prompt_text,
-      temperature=0.8,
+      temperature=0.9,
       max_tokens=150,
       top_p=1,
       frequency_penalty=0,
-      presence_penalty=0.3,
-      stop=["\n"],
+      presence_penalty=0.6,
+      stop=["\n"]
     )
     story = response['choices'][0]['text']
     return str(story)
